18eabf4689eaa82d13f929c4a8f6c072674d381c
Aleksandar Gakovic <algakovic@gmail.com>
2020-08-21 12:21:11 +0100

    add 30 second warning

--- timer.py
+++ timer.py
@@ -2,8 +2,12 @@
 # Simple timer program for feeding mags
 
 import time
-sec = 195
+sec = 45
 while sec > 0:
     print(sec)
+    
+    if sec == 30:
+        print('30 seconds to go...')
+
     time.sleep(1) # every seconds
     sec -= 1 # take on seconds away
