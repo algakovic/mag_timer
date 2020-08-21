c62883b209aca1b31aca74bb876a01c225f49ee5
Aleksandar Gakovic <algakovic@gmail.com>
2020-08-21 12:37:41 +0100

    add playsound file and alarm sound

--- timer.py
+++ timer.py
@@ -2,12 +2,16 @@
 # Simple timer program for feeding mags
 
 import time
-sec = 45
+from playsound import playsound
+
+sec = 5
 while sec > 0:
     print(sec)
     
     if sec == 30:
         print('30 seconds to go...')
+    if sec == 1:
+        playsound('C:/Users/kamik/code/projects/mag_timer/sounds/rose_confession.mp3')
 
     time.sleep(1) # every seconds
     sec -= 1 # take on seconds away
